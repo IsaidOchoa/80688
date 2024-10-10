1af6e5cf4467a6cccb459464a7a07d1d22835553
ochoatadeoaib.cbtis13@gmail.com <ochoatadeoaib.cbtis13@gmail.com>
2024-10-10 14:58:22 -0600

    Practica09

---
+++ practica09/src/MiFieldSet/MiFieldSet.jsx
@@ -0,0 +1,20 @@
+
+import "./MiFieldSet.css";
+function MiFieldSet(params) {
+    //p1 es el titulo
+    //
+    const {p1, id1, p2, id2, p3} = params
+    return (
+        <>
+    <fieldset>
+        <legend>{p1}</legend>
+        <label htmlFor={id1}>{p2}</label><br></br>
+        <input className="form-control" type="text" id="1" /><br></br>
+        <label htmlFor={id2}>{p3}</label><br></br>
+        <input className="form-control" type="text" id="2" />
+    </fieldset>
+        </>
+    )
+}
+
+export default MiFieldSet;
